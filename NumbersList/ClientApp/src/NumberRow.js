26b54cdfe11b7141fb6698e28b37e3defc83ca82
Malky Berger <malkylongbeach@gmail.com>
2022-05-04 17:54:37 -0400

    more code

--- NumbersList/ClientApp/src/NumberRow.js
+++ NumbersList/ClientApp/src/NumberRow.js
@@ -2,11 +2,11 @@ import react from "react";
 
 class NumberRow extends React.Component{
     render(){
-       const {numberString, id}= this.props.number
+       const {numberString}= this.props.number
 const {isSelected, isLocked} = this.props
        return(
            <tr>
-               <td>numberString</td>
+               <td>{numberString}</td>
                <td>
                    <button> {`btn btn-${isSelected ? 'danger' : 'primary'}`} disabled={isLocked} onClick={isSelected ? onUnselectClick : onSelectClick}
                         {isSelected ? 'Remove From Selected' : 'Add To Selected'}
