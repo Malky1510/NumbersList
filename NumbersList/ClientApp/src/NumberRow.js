f10fa229a7a3b3bfd5fe9c7bed2fc169608d9caf
Malky Berger <malkylongbeach@gmail.com>
2022-05-04 17:57:26 -0400

    more

--- NumbersList/ClientApp/src/NumberRow.js
+++ NumbersList/ClientApp/src/NumberRow.js
@@ -2,8 +2,8 @@ import react from "react";
 
 class NumberRow extends React.Component{
     render(){
-       const {numberString}= this.props.number
-const {isSelected, isLocked} = this.props
+        const { numberString } = this.props.number
+        const { isSelected, isLocked, onSelectClick, onUnselectClick } = this.props
        return(
            <tr>
                <td>{numberString}</td>
--- NumbersList/ClientApp/src/SelectedRow.js
+++ NumbersList/ClientApp/src/SelectedRow.js
@@ -4,10 +4,10 @@ class SelectedRow extends React.Component {
 
     render() {
         const { onLockClick, onUnlockClick, isLocked } = this.props;
-        const { number, id } = this.props.number;
+        const { numberString, id } = this.props.number;
 
         return (
-            <li className='list-group-item'>{number}     <button className='btn btn-primary' onClick={isLocked ? onUnlockClick : onLockClick}>
+            <li className='list-group-item'>{numberString}     <button className='btn btn-primary' onClick={isLocked ? onUnlockClick : onLockClick}>
                     {isLocked ? 'UNLOCK' : 'LOCK'}
                     </button>
             </li>
